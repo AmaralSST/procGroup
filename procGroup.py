01534a79ab7723d1fe3b38f4cd5851abab6956a0
AmaralSST <j.galvao@surgicalsafety.com>
2023-10-16 14:19:20 -0400

    Squashed commit of the following:
    commit 219d7e4b687b3b708072afc046dde2bc65c3de14
    Merge: e69e377 374859f
    Author: AmaralSST <j.galvao@surgicalsafety.com>
    Date:   Mon Oct 16 14:17:23 2023 -0400
    
        Merge branch 'master' into V2
    
    commit e69e37712625aafeeaee5ea7704f47764c125ebe
    Author: AmaralSST <j.galvao@surgicalsafety.com>
    Date:   Mon Oct 16 13:57:30 2023 -0400
    
        Updates and shortenning of PATH

--- procGroup.py
+++ procGroup.py
@@ -1,5 +1,25 @@
 import pandas as pd
 
+def replace_with_or(cell):
+    cell = str(cell)  # Convert to string just in case there are any float values
+    # Split the cell content by comma
+    codes = cell.split(',')
+    
+    codes = [code.strip() for code in codes]
+    # Replace the codes using the equivalents_map
+    replaced_codes = []
+    for code in codes:
+        code_stripped = code.strip()
+        if code_stripped in equivalents_map:
+            replaced_code = f"{code_stripped} OR {equivalents_map[code_stripped]}"
+            replaced_codes.append(replaced_code)
+        else:
+            replaced_codes.append(code_stripped)
+    
+    # Join the codes back into a single string with comma as the delimiter
+    return ','.join(replaced_codes)
+
+
 # Read the Excel file
 df = pd.read_excel('procGroup.xlsx', sheet_name='Sheet1')
 
@@ -7,13 +27,13 @@ df = pd.read_excel('procGroup.xlsx', sheet_name='Sheet1')
 df['bbp_name'] = df['bbp_name'].fillna('').astype(str)
 df['Procedure Name'] = df['Procedure Name'].fillna('').astype(str)
 
-# Group by 'Case ID' and aggregate the 'inter_procedure_code' and 'bbp_name' columns into sorted tuples
+# Group by 'Case ID' and aggregate the 'inter_procedure_code', 'bbp_name' and 'Procedure Name' columns into sorted tuples
 grouped_code = df.groupby('Case ID')['inter_procedure_code'].apply(lambda x: tuple(sorted(x))).reset_index()
-grouped_name = df.groupby('Case ID')['bbp_name'].apply(lambda x: tuple(sorted(x))).reset_index()
+grouped_bbp_name = df.groupby('Case ID')['bbp_name'].apply(lambda x: tuple(sorted(x))).reset_index()
 grouped_procedure_name = df.groupby('Case ID')['Procedure Name'].apply(lambda x: tuple(sorted(x))).reset_index()
 
-# Merge the two grouped dataframes on 'Case ID'
-merged_grouped = pd.merge(grouped_code, grouped_name, on='Case ID')
+# Merge grouped dataframes on 'Case ID'
+merged_grouped = pd.merge(grouped_code, grouped_bbp_name, on='Case ID')
 merged_grouped = pd.merge(merged_grouped, grouped_procedure_name, on='Case ID')
 
 # Group by the sorted tuple of 'inter_procedure_code' to get the Case IDs that share them
